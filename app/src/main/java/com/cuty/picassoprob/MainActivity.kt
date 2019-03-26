ef268ce87f16c0277840d1bb932f48ef2eb50b4f
Cuty <jchocobar886@gmail.com>
2019-03-26 13:53:19 -0300

    Initial commit

---
+++ app/src/main/java/com/cuty/picassoprob/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.cuty.picassoprob
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
+import android.widget.Toast
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.activity_main.*
+
+
+class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+
+    }
+    fun magicBu(view:View){
+        try{Picasso.with(this)
+                .load("https://i.imgur.com/H981AN7.jpg")
+                .into(iVMentalist)}
+        catch (ex:Exception){
+            Toast.makeText(this,"no hay conexion a internet",Toast.LENGTH_SHORT).show()
+
+        }
+    }
+
+
+
+
+}
